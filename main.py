9006bb056cec54369308acd4c308c00141f61d9d
Faizack <faizack619@gmail.com>
2024-05-21 02:16:08 +0530

    added data ingestion step

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from cnnClassifier.pipeline.stage_01_dataIngestion import DataIngestionTrainingPipeline
 from cnnClassifier import logger
 
-logger.info("Hello, world!")
+
+STAGE_NAME="DataIngestion"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion_training_pipeline = DataIngestionTrainingPipeline()
+        data_ingestion_training_pipeline.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
